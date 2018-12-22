b2bbd6d39b54b339b0b0da0ba07944ddfdb5cbac
jinho <jinho021712@gmail.com>
2018-12-22 18:59:26 +0900

    Update 파싱.py
     a태그 href값을 가져옴
    원하는 형태로 문자열을 자르고
    리스트에 삽입
    원하는 값'n'을 입력받고 'n'번째에 해당하는 리스트값을 출력

--- 파싱.py
+++ 파싱.py
@@ -1,20 +1,33 @@
+import urllib.parse
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
 search = input("검색어를 입력하세요 : " ) 
-
+search = urllib.parse.quote(search) #URL Encoding
 req = Request('https://torrentwal.net/bbs/s.php?k='+str(search)+'&q=', headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
 soup = BeautifulSoup(webpage,"lxml")
-num = 0
-print(req)
+num = 0  #num 초기값
+
+
 for link1 in soup.find_all(name="td",attrs={"class":"subject"}):
     title = link1.select('a')[1] #a태그중 0다음인 1번째 데이터 가져오기
     num = num+1
-    #print(link1.find('a').text)
-    
     print(str(num)+"번, "+title.get_text().strip()) #문자열만 가져오기
+mglist = [] * num #리스트 생성
 
-##유니코드 문제 해결
+for link2 in soup.find_all(name="td",attrs={"class":"num"}):
+    try:
+        mgnet = link2.select('a')[0]['href'] #a 태그에서 href만 가져오기
+    except:
+        pass
+        #print("href 없음")
+    mgnet = mgnet.strip("javascript:Mag_dn('')") #문자열 자르기
+    mglist.append(str(mgnet)) #리스트에 넣기
+
+mgnum = int(input("번호를 선택하세요 : " ))
+print(mglist[mgnum])
+
 
 ##마그넷 형식 magnet:?xt=urn:btih:68375FAE44313634251CD075EF617E8859319F63
+
